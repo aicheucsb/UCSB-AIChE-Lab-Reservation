933befa4a8706d6b4f103a7df7aee3adbc3a3657
Dylan Vu <dylanvu92gmail.com>
2021-12-26 00:36:44 -0800

    attempt to solve CORs

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://ucsb-aiche-lab-reservation.vercel.app/api/:path*',
+      },
+    ]
+  },
 }
