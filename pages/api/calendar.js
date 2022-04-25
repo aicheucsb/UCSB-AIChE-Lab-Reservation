3487b97c6bb7c62fb99831a65365efd2d2a2053c
Dylan Vu <dylanvu92gmail.com>
2022-04-24 23:53:02 -0700

    try to fix error after accounting for dst

--- pages/api/calendar.js
+++ pages/api/calendar.js
@@ -32,8 +32,8 @@ function handler(req, res) {
         body = req.body;
     }
     console.log("Got new request: ", body);
-    const startTime = adjustForDST(body.start); // body.start looks like: '2022-04-25T07:06:00-08:00' YYYY-MM-DDTHH:mm:ssZ format
-    const endTime = adjustForDST(body.end);
+    const startTime = body.start; // these look like: '2022-04-25T07:06:00-08:00' YYYY-MM-DDTHH:mm:ssZ format
+    const endTime = body.end;
     const description = "Note: Please ensure you've completed the safety training and wear long sleeved clothes and closed toe shoes if you will be working with chemicals.\n\n" + body.description;
     const title = body.title;
 
@@ -43,12 +43,16 @@ function handler(req, res) {
         // CalendarID is needed to get the calendar
         // If you delete and remake the calendar, you must get the ID again using API Explorer and paste it in inside of const calendarID
 
+        // Convert to the google calendar friendly time while checking if DST
+        const momentStart = adjustForDST(startTime);
+        const momentEnd = adjustForDST(endTime);
+
         // Check availability of the time
-        CheckAvailability(res, calendarID, accessToken, startTime, endTime).then((available) => {
+        CheckAvailability(res, calendarID, accessToken, momentStart, momentEnd).then((available) => {
             if (available) {
                 // Make the reservation
-                MakeReservation(res, calendarID, accessToken, startTime, endTime, description, title).then(() => {
-                    SendConfirmationEmail(body.email, body.name, startTime, endTime);
+                MakeReservation(res, calendarID, accessToken, momentStart, momentEnd, description, title).then(() => {
+                    SendConfirmationEmail(body.email, body.name, momentStart, momentEnd);
                 });
             } else {
                 res.status(409).send('Conflicting times with another reservation'); // 409: conflict status code. There was a conflict with overlapping times. Likely not an issue with the code
